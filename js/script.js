e964609a650480069f92c55586f75a53dff39e08
alesya-zagorovskaya <alesya.zagorovskaya@gmail.com>
2019-07-28 22:21:16 +0300

    version 14

--- js/script.js
+++ js/script.js
@@ -18,7 +18,21 @@ jQuery(function($) {
         }
     });
 
-    $(".button-up").on('click', window.scrollTo(0,0));
+    $(document).ready(function(){
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 0) {
+                $('.button-up').fadeIn();
+            } else {
+                $('.button-up').fadeOut();
+            }
+        });
+        $('.button-up').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 1500);
+            return false;
+        });
+    });
 
     $('.line-menu a').on( 'click', function(){
         var el = $(this);
